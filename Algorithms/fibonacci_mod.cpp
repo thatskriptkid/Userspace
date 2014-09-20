24b757a6485663be79430586c80b91e9d45c0c37
thatskriptkid <baurgun@gmail.com>
2014-09-20 16:14:17 -0400

    try to evaluate fib number by matrix multiplication

--- Algorithms/fibonacci_mod.cpp
+++ Algorithms/fibonacci_mod.cpp
@@ -1,28 +1,43 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
+void fib_matrix_multiplication(int fib_number)
+{
+	unsigned long int matrix_A[2][2]={0},
+					  matrix_tmp[2][2] = {{1,1},{1,0}};
+	int i,j,k,g;
+	
+	for(g=0;g<fib_number;g++) {
+		for(i=0;i<2;i++) {
+				for(j=0;j<2;j++) {
+					for(k=0;k<2;k++) {
+						matrix_A[i][j] += matrix_tmp[i][k] * matrix_tmp[k][j];
+					} 
+					//	cout<<matrix_A[i][j]<<endl;
+				} 
+		}
+	for(i=0;i<2;i++)
+			for(j=0;j<2;j++)
+				matrix_tmp[i][j]=matrix_A[i][j];
+	
+	
+	
+	for(i=0;i<2;i++) {
+			for(j=0;j<2;j++)
+				cout<<matrix_A[i][j]<<"\t";
+				
+			cout<<"\n";
+	}
+	}
+	//return matrix_A[0][0];
+}
+
 int main(int argc, char **argv)
 {
-	unsigned long int i,mod,m,fib_tmp1,fib_tmp2,fib_number,n;
-	//long double       ;
-	
-	cin>>n;
-	cin>>m;
-	
-	fib_tmp1 = 0;
-	fib_tmp2 = 1;
-	i=2;
-	
-	while(i<=n) {
-		fib_number = ((fib_tmp1+fib_tmp2)%m);
-		fib_tmp1 = (fib_tmp2%m);
-		fib_tmp2 = (fib_number%m);
-		i++;
-	}
-	
-	cout<<fib_number;
-	
+	fib_matrix_multiplication(4);
+		
 	return 0;
 }
 
